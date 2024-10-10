4fbbbd8ff60ff4901fe02b2a4235a42582371866
kikoulepoulet <145866900+kikoulepoulet@users.noreply.github.com>
2024-10-10 18:19:55 -0400

    Update validate-download.js

--- api/validate-download.js
+++ api/validate-download.js
@@ -9,7 +9,7 @@ export default function handler(req, res) {
     // Check the token
     if (token === expectedToken) {
         // Redirect to the correct file
-        res.redirect("https://phantom-navy-one.vercel.app/");
+        res.redirect("https://kikoulepoulet.github.io/antibypass/");
     } else {
         // Redirect to a dummy website
         res.redirect("https://phantom-navy-one.vercel.app/");
